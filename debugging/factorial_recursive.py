b28495f863d1b6d2b5a683afa8b32c967893a7eb
alaa2026ali <ali_alaa18@icloud.com>
2026-06-02 11:32:47 +0300

    Refactor factorial function and add main guard
    Removed docstring from the factorial function and added a main guard.

--- debugging/factorial_recursive.py
+++ debugging/factorial_recursive.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
 import sys
 
+
 def factorial(n):
-    """
-    Function Description:
-        Calculates the factorial of a number using recursion.
-
-    Parameters:
-        n (int): The number for which the factorial is calculated.
-
-    Returns:
-        int: The factorial of the given number.
-    """
     if n == 0:
         return 1
     else:
         return n * factorial(n - 1)
 
-f = factorial(int(sys.argv[1]))
-print(f)
+
+if __name__ == "__main__":
+    f = factorial(int(sys.argv[1]))
+    print(f)
